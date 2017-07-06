5253e72401e957473e89ed07defc49bb0792edfd
Jon Coello <joncoello@gmail.com>
2017-07-06 20:31:42 +0100

    update title test

--- App1/App1/ViewModels/ItemsViewModel.cs
+++ App1/App1/ViewModels/ItemsViewModel.cs
@@ -17,7 +17,7 @@ namespace App1.ViewModels
 
 		public ItemsViewModel()
 		{
-			Title = "Browse";
+			Title = "BrowseZZ";
 			Items = new ObservableRangeCollection<Item>();
 			LoadItemsCommand = new Command(async () => await ExecuteLoadItemsCommand());
 
